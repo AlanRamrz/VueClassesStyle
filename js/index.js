019cade8ac85c2e907acf36b09ad2b21669e9f69
Alan Ramirez <alanramirez@MacBook-Pro-de-Alan.local>
2018-09-13 16:00:03 -0500

    Add 4 basic examples with classes and style.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,32 @@
 var ex1 = new Vue({
   el: '#ex1',
   data: {
-    message: 'Hello world'
+    isDone: true
+  }
+})
+
+var ex2 = new Vue({
+  el: '#ex2',
+  data: {
+    isDone: true,
+    withColorRed: true
+  }
+})
+
+var ex3 = new Vue({
+  el: '#ex3',
+  data: {
+    myObject: {
+      //This is the name of the class
+      done: true,
+      red: true
+    }
+  }
+})
+
+var ex4 = new Vue({
+  el: '#ex4',
+  data: {
+    number: 50
   }
 })
